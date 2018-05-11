31233833c273d13d457404ac7f660fc8cc1b53d9
Dan Koenig <dkoenig@gongos.com>
2018-05-11 15:43:27 -0400

    vue stuff
    Signed-off-by: Dan Koenig <dkoenig@gongos.com>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # vuebootcamppart3
 
 
-'vue-cli-project' is an npm supported Vue js project which functions as the front-end
+'vuefrontend' is an npm supported Vue js project which functions as the front-end
 
-gameservices is a flask application which functions as the back-end
+'gameservices' is a flask application which functions as the back-end
---
+++ vuefrontend/src/UIDrawerComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-bind:class="open ? 'ui-drawer open' : 'ui-drawer'">
+    <label class="open-toggle">
+      <input type="checkbox" v-model="open"/>
+      <slot name="titletext"></slot>
+    </label>
+    <slot name="userinterface"></slot>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      open: false
+    }
+  }
+}
+</script>
+
