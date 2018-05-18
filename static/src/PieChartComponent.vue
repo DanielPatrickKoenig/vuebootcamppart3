ff8127ef3e2f28d7075386e3fc9b4a48bf99c684
Dan Koenig <dkoenig@gongos.com>
2018-05-18 16:07:29 -0400

    working in flask
    Signed-off-by: Dan Koenig <dkoenig@gongos.com>

---
+++ static/src/PieChartComponent.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="pie-chart">
+    <div v-bind:id="chartid"></div>
+  </div>
+</template>
+<script>
+  import echarts from 'echarts'
+  export default {
+    data () {
+      return {
+        message: 'sup dude',
+        chartid: 'CHART_' + Math.random().toString().split('.').join('') + Math.random().toString().split('.').join('') + Math.random().toString().split('.').join(''),
+        chartObject: undefined
+      }
+    },
+    props: ['chartdata', 'colors', 'textcolor', 'title', 'hovertitle'],
+    watch: {
+      chartdata: function () {
+        var self = this
+        if (document.getElementById(self.$data.chartid) !== undefined) {
+          var _data = []
+          var _values = []
+          var _keys = []
+          console.log(this.chartdata)
+          for (var k in this.chartdata) {
+            _keys.push(k)
+            _values.push(this.chartdata[k])
+            _data.push({name: k, value: this.chartdata[k]})
+          }
+          var option = {
+            title: {
+              text: this.title,
+              textStyle: {
+                color: this.textcolor
+              },
+              x: 'left'
+            },
+            color: this.colors,
+            tooltip: {
+              trigger: 'item',
+              formatter: '{a} <br/>{b}: {c} ({d}%)'
+            },
+            legend: {
+              orient: 'vertical',
+              x: 'left',
+              y: 'center',
+              data: _keys,
+              textStyle: {
+                color: this.textcolor
+              }
+            },
+            series: [
+              {
+                name: this.hovertitle,
+                type: 'pie',
+                radius: ['50%', '70%'],
+                avoidLabelOverlap: false,
+                label: {
+                  normal: {
+                    show: false,
+                    position: 'center'
+                  },
+                  emphasis: {
+                    show: true,
+                    textStyle: {
+                      fontSize: '30',
+                      fontWeight: 'bold'
+                    }
+                  }
+                },
+                labelLine: {
+                  normal: {
+                    show: false
+                  }
+                },
+                data: _data
+              }
+            ]
+          }
+          console.log(self.$data.chartObject)
+          self.$data.chartObject.setOption(option)
+        }
+      }
+    },
+    mounted: function () {
+      var self = this
+      self.$data.chartObject = echarts.init(document.getElementById(self.$data.chartid))
+    }
+  }
+</script>
---
+++ static/src/RatingChartComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="rating-chart" style="position:relative;">
+    <div v-bind:style="'position:absolute;height:100%;width:'+((chartdata/total)*100)+'%;background-color:'+color">
+    </div>
+    <span v-text="text" style="position:relative;"></span>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  props: ['chartdata', 'text', 'total', 'color']
+}
+</script>
